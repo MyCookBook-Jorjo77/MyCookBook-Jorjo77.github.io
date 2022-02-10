97d54cb8c6c269bb5ee40e2b29bad88a96663e00
Georgi Kolev <tuvkontrol55@gmail.com>
2022-02-10 11:36:53 +0200

    Update library urls

--- src/src/views/auth.js
+++ src/src/views/auth.js
@@ -1,4 +1,6 @@
-import { html } from '../../node_modules/lit-html/lit-html.js';
+//import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import { html } from './lib.js';
 
 import { login, register } from '../api/data.js';
 
